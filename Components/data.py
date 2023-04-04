37cb33e554475a608aa0c7f3eb4b3d1197fd8500
Yushan Wang <yushanwang0816@gmail.com>
2023-04-05 05:42:53 +0800

    leader board template done

---
+++ Components/data.py
@@ -0,0 +1,42 @@
+leaderboard = [
+  {
+    "team": 'RED BULL RACING HONDA RBPT',
+    "pts": 123,
+  },
+  {
+    "team": 'ASTON MARTIN ARAMCO MERCEDES',
+    "pts": 123,
+  },
+  {
+    "team": 'MERCEDES',
+    "pts": 123,
+  },
+  {
+    "team": 'FERRARI',
+    "pts": 123,
+  },
+  {
+    "team": 'MCLAREN MERCEDES',
+    "pts": 123,
+  },
+  {
+    "team": 'ALPINE RENAULT',
+    "pts": 123,
+  },
+  {
+    "team": 'HAAS FERRARI',
+    "pts": 123,
+  }, 
+  {
+    "team": 'ALFA ROMEO FERRARI',
+    "pts": 123,
+  },
+  {
+    "team": 'ALPHATAURI HONDA RBPT',
+    "pts": 123,
+  },
+  {
+    "team": 'WILLIAMS MERCEDES',
+    "pts": 123,
+  },
+]
--- run.py
+++ run.py
@@ -1,40 +1,7 @@
 from flask import Flask, render_template, url_for
+from Components.data import leaderboard
 
 app = Flask(__name__)
-
-
-leaderboard = [
-  {
-    "team": 'RED BULL RACING HONDA RBPT',
-  },
-  {
-    "team": 'ASTON MARTIN ARAMCO MERCEDES',
-  },
-  {
-    "team": 'MERCEDES',
-  },
-  {
-    "team": 'FERRARI',
-  },
-  {
-    "team": 'MCLAREN MERCEDES',
-  },
-  {
-    "team": 'ALPINE RENAULT',
-  },
-  {
-    "team": 'HAAS FERRARI',
-  }, 
-  {
-    "team": 'ALFA ROMEO FERRARI',
-  },
-  {
-    "team": 'ALPHATAURI HONDA RBPT',
-  },
-  {
-    "team": 'WILLIAMS MERCEDES',
-  },
-]
 
 @app.route("/")
 @app.route("/home")
