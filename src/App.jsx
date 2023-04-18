1c285f0616ffe97720d6fa33b050e36a2f09a6ac
Yushan Wang <yushanwang0816@gmail.com>
2023-04-19 02:09:56 +0800

    initialize react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <header aria-label="Masthead">
+      <h1>
+        <small>Yushan Wang&rsquo;s</small>
+        <span>Formula 1<br />Dashboard</span>
+      </h1>
+    </header>
+  );
+}
+
+export default App
