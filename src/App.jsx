e8fd62d3ae1bc1c039c80d0340b971df6859618a
Yushan Wang <yushanwang0816@gmail.com>
2023-04-24 06:15:12 +0800

    update IconSection

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,10 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faMedium, faInstagram, faLinkedin }
             from '@fortawesome/free-brands-svg-icons';
+import { faArrowsRotate, faCopy } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    // <div className="app">
-    //   <div className="title">
-    //     <h1>
-    //       <a href="http://yushanwang9801.github.io">
-    //         <small>Yushan Wang&rsquo;s</small><br />
-    //       </a>
-    //       <div>Formula 1 Dashboard</div>
-    //     </h1>
-    //   </div>
-    //   <div className="colorboard">
-    //     {data.map((d) => <Color team={d} />)}
-    //   </div>
-    // </div>
     <div className="app">
       <div className="title">
         <h1>
@@ -35,48 +23,56 @@ function App() {
         {data.map((d) => <Color team={d} />)}
       </div>
       <div className="iconSection">
-          {/* <a href="http://yushanwang9801.github.io">YushanWang9801</a> */}
           <ul>
-          <li>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span><a href="https://github.com/YushanWang9801"
-            target="_blank" rel="noreferrer" aria-label="Find me on Github">
-            <FontAwesomeIcon className="faicon" icon={faGithub} />
-            </a></span>
-          </li>
-          <li>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span><a href="https://github.com/YushanWang9801"
-            target="_blank" rel="noreferrer" aria-label="Find me on Github">
-            <FontAwesomeIcon className="faicon" icon={faGithub} />
-            </a></span>
-          </li>
-          <li>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span><a href="https://github.com/YushanWang9801"
-            target="_blank" rel="noreferrer" aria-label="Find me on Github">
-            <FontAwesomeIcon className="faicon" icon={faGithub} />
-            </a></span>
-          </li>
-          <li>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span><a href="https://github.com/YushanWang9801"
-            target="_blank" rel="noreferrer" aria-label="Find me on Github">
-            <FontAwesomeIcon className="faicon" icon={faGithub} />
-            </a></span>
-          </li>
+          <Icon icon={faCopy} 
+                  link={"#"} 
+                  text={"#"}/>
+          <Icon icon={faArrowsRotate} 
+                  link={"#"} 
+                  text={"#"}/>
+          <Icon icon={faInstagram} 
+                link={"https://www.instagram.com/yushan9801/"} 
+                text={"Find me on Github"}/>
+          <Icon icon={faMedium} 
+                link={"https://medium.com/@wang33he77"} 
+                text={"Find me on Medium"}/>
+          <Icon icon={faGithub} 
+                link={"https://github.com/YushanWang9801"} 
+                text={"Find me on Instagram"}/>
           </ul>
       </div>
     </div>
   );
+}
+
+
+function Icon({icon, link, text}){
+  if(link === "#"){
+      return (
+        <li>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span>
+          <FontAwesomeIcon className="faicon" icon={icon} />
+        </span>
+      </li>
+    );
+  } else {
+    return (
+      <li>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span>
+          <a href={link}
+        target="_blank" rel="noreferrer" aria-label={text}>
+        <FontAwesomeIcon className="faicon" icon={icon} />
+        </a>
+      </span>
+    </li>
+    );
+  }
 }
 
 export default App
